c6060f437853fd32d2a32b2799f0451210601dba
Orlando Mario Arias <orlando.arias@mail.citytech.cuny.edu>
2018-03-19 05:43:29 -0400

    Consistent spaces

--- generator.py
+++ generator.py
@@ -4,7 +4,7 @@ import csv
 # Define teams
 teams = ['Team 1', 'Team 2', 'Team 3', 'Team 4', 'Team 5', 'Team 6', 'Team 7','Team 8','Team 9','Team 10']
 
-#Iterate through teams
+# Iterate through teams
 matches = list(itertools.permutations(players, 2))
 
 # Print matches
